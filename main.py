46a6c21007a8bc1470b9ba8f1beea39208e51d85
cloudoutloud <39462069+cloudoutloud@users.noreply.github.com>
2022-09-04 13:08:52 +0100

    Add for loop, functions and udpate readme

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import boto3
 import boto3.session
 import json
 from jsonpath import JSONPath
+import yaml
 
 # # If using default profile uncomment line 10,11
 # my_profile_name = ""
@@ -13,38 +14,59 @@ from jsonpath import JSONPath
 
 # Getting AWS account id from assumed profile
 account_id = boto3.client('sts').get_caller_identity().get('Account')
-# Will only wokr if account is member of a org
-#account_name = boto3.client('organizations').describe_account(AccountId=account_id).get('Account').get('Name')
-print("Using account id: " + account_id)
+# Will only work if account is member of an org
+# Account_name = boto3.client('organizations').describe_account(AccountId=account_id).get('Account').get('Name') TODO
+# print("Using account id: " + account_id)
 
 ## List EKS and get latest version TODO
 # eks = boto3.client('eks')
 # eks_versions = eks.describe_addon_versions()
 
-## List Managed service Kafka and get latest version
-msk = boto3.client('kafka')
-msk_versions = msk.list_kafka_versions()
-# print(json.dumps(msk_versions, indent = 1))
-msk_latest_version = msk_versions["KafkaVersions"][-1]["Version"]
-print("Latest Managed Service Kafka version is: " + msk_latest_version)
+def kafka():
+    ## List Managed service Kafka and get latest version
+    msk = boto3.client('kafka')
+    msk_versions = msk.list_kafka_versions()
+    # print(json.dumps(msk_versions, indent = 1))
+    msk_latest_version = msk_versions["KafkaVersions"][-1]["Version"]
+    print("Latest Managed Service Kafka version is: " + msk_latest_version)
 
-# Loop through MSK clusters and list ClusterName and CurrentVersion
-msk_clusters = msk.list_clusters_v2()
-# print(msk_clusters)
+    # Loop through MSK clusters and list ClusterName and CurrentVersion
+    msk_clusters = msk.list_clusters_v2()
+    if len(msk_clusters["ClusterInfoList"]) == 0:
+        print("No MSK clusters found in account skipping....")
+    else:
+        print(yaml.dump(msk_clusters["ClusterInfoList"], sort_keys=False, default_flow_style=False))
+    # Find version and of cluster and compare with msk_latest_version TODO
 
-## List Elastic search versions and get latest version
-es = boto3.client('opensearch')
-es_verisons = es.list_versions()
-# print(json.dumps(es_verisons, indent = 1))
-es_latest_version = es_verisons["Versions"][0]
-print("Latest Elastic Search version is: " + es_latest_version)
+def es():
+    ## List Elastic search versions and get latest version
+    es = boto3.client('opensearch')
+    es_verisons = es.list_versions()
+    # print(json.dumps(es_verisons, indent = 1))
+    es_latest_version = es_verisons["Versions"][0]
+    print("Latest Elastic Search version is: " + es_latest_version)
 
-## List RDS versions and get latest version
-rds = boto3.client('rds')
-rds_verisons = rds.describe_db_engine_versions()
-# print(json.dumps(rds_verisons, indent = 1))
-aurora_mysql_latest_version = JSONPath('$.DBEngineVersions[?(@.Engine=="aurora-mysql")].EngineVersion').parse(rds_verisons)[0]
-print("Latest Aurora MYSQL version is: " + aurora_mysql_latest_version)
-aurora_mysql_latest_version = JSONPath('$.DBEngineVersions[?(@.Engine=="aurora-postgresql")].EngineVersion').parse(rds_verisons)[-1]
-print("Latest Aurora Postgresql version is: " + aurora_mysql_latest_version)
+    es_domain_names = es.list_domain_names()
+    if len(es_domain_names["DomainNames"]) == 0:
+        print("No Elastic Search domains found in account skipping....")
+    else:
+        print(yaml.dump(es_domain_names["DomainNames"], sort_keys=False, default_flow_style=False))
 
+def rds():
+    ## List RDS versions and get latest version
+    rds = boto3.client('rds')
+    rds_verisons = rds.describe_db_engine_versions()
+    # print(json.dumps(rds_verisons, indent = 1))
+    aurora_mysql_latest_version = JSONPath('$.DBEngineVersions[?(@.Engine=="aurora-mysql")].EngineVersion').parse(rds_verisons)[0]
+    print("Latest Aurora MYSQL version is: " + aurora_mysql_latest_version)
+    aurora_mysql_latest_version = JSONPath('$.DBEngineVersions[?(@.Engine=="aurora-postgresql")].EngineVersion').parse(rds_verisons)[-1]
+    print("Latest Aurora Postgresql version is: " + aurora_mysql_latest_version)
+
+for accounts in account_id:
+    print("Using account id: " + account_id)
+    kafka()
+    es()
+    rds()
+    break
+else:
+    print("No aws accounts configured...")
